762c23053a2cb9786b12bb3c30670b5576c3e385
galemagalhaes <g.alemagalhaes@gmail.com>
2021-04-29 20:49:44 -0300

    correções

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import {GetStaticProps} from 'next';
 import Image from 'next/image'; 
 import Link from 'next/link';
 import {format, parseISO} from 'date-fns';
-import ptBR from 'date-fns/locale/pt-BR'
+import ptBR from 'date-fns/locale/pt-BR';
 import { api } from '../services/api';
 import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
 import styles from './home.module.scss';
