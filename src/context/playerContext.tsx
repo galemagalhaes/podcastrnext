107f6f00a59d67d9d4a0fc7b82b99889ac712ad5
galemagalhaes <g.alemagalhaes@gmail.com>
2021-04-30 19:09:15 -0300

    Finalizando o projeto
    Finalização das funções do player, botões, uso de métodos da tag html audio, alterações de estilo. Completando contexto. Atualização do head.

--- src/context/playerContext.tsx
+++ src/context/playerContext.tsx
@@ -1,5 +1,5 @@
-import { type } from 'node:os';
-import {createContext, useState, ReactNode} from 'react';
+//import { type } from 'node:os';
+import {createContext, useState, ReactNode, useContext} from 'react';
 
 type Episode = {
   title: string;
@@ -13,10 +13,19 @@ type playerContextData = {
   episodeList: Episode[];
   currentEpisodeIndex: number;
   isPlaying: boolean;
+  isLooping: boolean;
+  isShuffling: boolean;
   play: (episode: Episode) => void;
   playList: (list: Episode[], index: number) => void;
   setPlayingState: (state: boolean) => void;
   togglePlay: () => void;
+  toggleLoop: () => void;
+  toggleShuffle: () => void;
+  playNext: () => void;
+  playPrevious: () => void;
+  clearPlayerState: () => void;
+  hasNext: boolean;
+  hasPrevious: boolean;
 };
 
 export const playerContext = createContext({} as playerContextData);
@@ -28,7 +37,9 @@ type PlayerContextProviderProps = {
 export function PlayerContextProvider({children}: PlayerContextProviderProps){
   const [episodeList, setEpisodeList] = useState([]);
   const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
-  const [isPlaying, setIsPlaying] = useState(false)
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [isLooping, setIsLooping] = useState(false);
+  const [isShuffling, setIsShuffling] = useState(false);
 
   function play(episode: Episode) {
     setEpisodeList([episode]);
@@ -46,23 +57,67 @@ export function PlayerContextProvider({children}: PlayerContextProviderProps){
     setIsPlaying(!isPlaying)
   }
 
+  function toggleLoop() {
+    setIsLooping(!isLooping)
+  }
+
+  function toggleShuffle() {
+    setIsShuffling(!isShuffling)
+  }
+
   function setPlayingState(state: boolean) {
     setIsPlaying(state);
   }
 
-  return (
+  function clearPlayerState() {
+    setEpisodeList([]);
+    setCurrentEpisodeIndex(0);
+  }
+
+  const hasPrevious = currentEpisodeIndex > 0
+  const hasNext = isShuffling || (currentEpisodeIndex + 1) < episodeList.length
+
+  function playNext() {
+    if(isShuffling) {
+      const nextRandomEpisodeIndex = Math.floor(Math.random() * episodeList.length)
+      setCurrentEpisodeIndex(nextRandomEpisodeIndex);
+    } else if (hasNext) { 
+      setCurrentEpisodeIndex(currentEpisodeIndex + 1);
+    }    
+  }
+
+  function playPrevious() {
+    if(hasPrevious) {
+    setCurrentEpisodeIndex(currentEpisodeIndex - 1);
+    }
+  }
+
+    return (
     <playerContext.Provider 
     value={{
       episodeList,
       currentEpisodeIndex, 
       play, 
       playList,
-      isPlaying, 
+      playPrevious,
+      playNext,
+      isPlaying,
+      isLooping, 
+      isShuffling,
       togglePlay, 
-      setPlayingState
+      toggleLoop,
+      toggleShuffle,
+      setPlayingState,
+      hasPrevious,
+      hasNext,
+      clearPlayerState,
       }}
       >
 
       {children}
     </playerContext.Provider>
   )}
+
+  export const usePlayer = () => {
+    return useContext(playerContext)
+  }
--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -2,11 +2,13 @@ import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR'
 import { GetStaticPaths, GetStaticProps } from 'next';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
 import { api } from '../../services/api';
 import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
 import styles from './episode.module.scss';
 import Image from 'next/image'
+import { usePlayer } from '../../context/playerContext';
+import React from 'react';
+import Head from 'next/head';
 
 type Episode = {
   id: string;
@@ -25,11 +27,13 @@ type EpisodeProps = {
 }
 
 export default function Episode({ episode }: EpisodeProps) {
-  const router = useRouter();
-
-  
+  const { play } = usePlayer();
+    
   return (
     <div className={styles.episode}>
+       <Head>
+        <title>{episode.title} | Podcastr</title>
+      </Head>
       <div className={styles.thumbnailContainer}>
         <Link href="/">
         <button type="button">
@@ -42,7 +46,7 @@ export default function Episode({ episode }: EpisodeProps) {
         src={episode.thumbnail} 
         objectFit="cover" />
 
-        <button type = "button">
+        <button type = "button" onClick = {() => play(episode)}>
           <img src="/play.svg" alt="Tocar episódio"/>
         </button>
       </div>
